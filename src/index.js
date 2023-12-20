be0b9134a7ac9d4b3f3c94d90f6ba358db92f99b
Denis Magarshak <d.magarshak@gmail.com>
2023-12-20 22:24:41 +0300

    import & install lodash

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+
+// Вывод текста
+console.log('Hello, Hexlet!');
+
+// Пересечение массивов
+console.log(_.intersection([2, 1], [2, 3])); // 2
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello")); // Hello
+
+// Вывод последнего значения
+console.log(_.last(['one', 'two']));
